31cd2abc94fa504ed92ed57b0853724da688e9bc
William Herrera <william.herrera@kofax.com>
2019-10-05 20:12:03 -0700

    finished colors

--- app/src/main/java/com/herrera/william/gridlayout/DemoGridLayout.java
+++ app/src/main/java/com/herrera/william/gridlayout/DemoGridLayout.java
@@ -24,74 +24,74 @@ public class DemoGridLayout extends Activity {
         layout.setLayoutParams(params);
 
         // Row 1
-        createWidgetAtCell(layout, "1", 0, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "1", 0, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "Rules void hello1(int hour)", 0, 1, 24, 1f);
 
         // Row 2
-        createWidgetAtCell(layout, "2", 1, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "2", 1, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "properties", 1, 1, 4, 1f);
         createWidgetAtCell(layout, "name", 1, 5, 10, 1f);
         createWidgetAtCell(layout, "Day Hour Classification", 1, 15, 10, 1f);
 
         // Row 3
-        createWidgetAtCell(layout, "3", 2, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "3", 2, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "properties", 2, 1, 4, 1f);
         createWidgetAtCell(layout, "category", 2, 5, 10, 1f);
         createWidgetAtCell(layout, "Day and Time", 2, 15, 10, 1f);
 
         // Row 4
-        createWidgetAtCell(layout, "4", 3, 0, 1, 0f);
-        createWidgetAtCell(layout, "Rule", 3, 1, 4, 1f);
-        createWidgetAtCell(layout, "C1", 3, 5, 10, 1f);
-        createWidgetAtCell(layout, "A1", 3, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "4", 3, 0, 1, 0f, "#9c908f");
+        createWidgetAtCellWithColor(layout, "Rule", 3, 1, 4, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "C1", 3, 5, 10, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "A1", 3, 15, 10, 1f, "#26fcf9");
 
         // Row 5
-        createWidgetAtCell(layout, "5", 4, 0, 1, 0f);
-        createWidgetAtCell(layout, "", 4, 1, 4, 1f);
-        createWidgetAtCell(layout, "min<=hour && hour<=max", 4, 5, 10, 1f);
-        createWidgetAtCell(layout, "System.out.println(greeting + , World!)", 4, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "5", 4, 0, 1, 0f, "#9c908f");
+        createWidgetAtCellWithColor(layout, "", 4, 1, 4, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "min<=hour && hour<=max", 4, 5, 10, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "System.out.println(greeting + , World!)", 4, 15, 10, 1f, "#26fcf9");
 
         // Row 6
-        createWidgetAtCell(layout, "6", 5, 0, 1, 0f);
-        createWidgetAtCell(layout, "", 5, 1, 4, 1f);
-        createWidgetAtCell(layout, "int min", 5, 5, 5, 1f);
-        createWidgetAtCell(layout, "int max", 5, 10, 5, 1f);
-        createWidgetAtCell(layout, "String Greeting", 5, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "6", 5, 0, 1, 0f, "#9c908f");
+        createWidgetAtCellWithColor(layout, "", 5, 1, 4, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "int min", 5, 5, 5, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "int max", 5, 10, 5, 1f, "#26fcf9");
+        createWidgetAtCellWithColor(layout, "String Greeting", 5, 15, 10, 1f, "#26fcf9");
 
         // Row 7
-        createWidgetAtCell(layout, "7", 6, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "7", 6, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "Rule", 6, 1, 4, 1f);
-        createWidgetAtCell(layout, "From", 6, 5, 5, 1f);
-        createWidgetAtCell(layout, "To", 6, 10, 5, 1f);
-        createWidgetAtCell(layout, "Greeting", 6, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "From", 6, 5, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "To", 6, 10, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "Greeting", 6, 15, 10, 1f, "#fcb858");
 
         // Row 8
-        createWidgetAtCell(layout, "8", 7, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "8", 7, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "R10", 7, 1, 4, 1f);
-        createWidgetAtCell(layout, "0", 7, 5, 5, 1f);
-        createWidgetAtCell(layout, "11", 7, 10, 5, 1f);
-        createWidgetAtCell(layout, "Good Morning", 7, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "0", 7, 5, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "11", 7, 10, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "Good Morning", 7, 15, 10, 1f, "#fcb858");
 
         // Row 9
-        createWidgetAtCell(layout, "9", 8, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "9", 8, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "R20", 8, 1, 4, 1f);
-        createWidgetAtCell(layout, "12", 8, 5, 5, 1f);
-        createWidgetAtCell(layout, "17", 8, 10, 5, 1f);
-        createWidgetAtCell(layout, "Good Afternoon", 8, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "12", 8, 5, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "17", 8, 10, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "Good Afternoon", 8, 15, 10, 1f, "#fcb858");
 
         // Row 10
-        createWidgetAtCell(layout, "10", 9, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "10", 9, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "R30", 9, 1, 4, 1f);
-        createWidgetAtCell(layout, "18", 9, 5, 5, 1f);
-        createWidgetAtCell(layout, "21", 9, 10, 5, 1f);
-        createWidgetAtCell(layout, "Good Evening", 9, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "18", 9, 5, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "21", 9, 10, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "Good Evening", 9, 15, 10, 1f, "#fcb858");
 
         // Row 11
-        createWidgetAtCell(layout, "11", 10, 0, 1, 0f);
+        createWidgetAtCellWithColor(layout, "11", 10, 0, 1, 0f, "#9c908f");
         createWidgetAtCell(layout, "R40", 10, 1, 4, 1f);
-        createWidgetAtCell(layout, "22", 10, 5, 5, 1f);
-        createWidgetAtCell(layout, "23", 10, 10, 5, 1f);
-        createWidgetAtCell(layout, "Good Night", 10, 15, 10, 1f);
+        createWidgetAtCellWithColor(layout, "22", 10, 5, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "23", 10, 10, 5, 1f, "#fcfc7e");
+        createWidgetAtCellWithColor(layout, "Good Night", 10, 15, 10, 1f, "#fcb858");
 
         setContentView(layout);
 
@@ -103,6 +103,21 @@ public class DemoGridLayout extends Activity {
         TextView tv = new TextView(this);
         tv.setText(s);
         tv.setBackgroundColor(Color.WHITE);
+        GridLayout.Spec rowSpec = GridLayout.spec(row);
+        GridLayout.Spec colSpec = GridLayout.spec(col, size, weight);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, colSpec);
+        params.setMargins(1, 1, 1, 1);
+        tv.setLayoutParams(params);
+        g.addView(tv);
+
+    }
+
+    private void createWidgetAtCellWithColor(GridLayout g, String s, int row, int col, int size, float weight, String hexColor)
+    {
+        TextView tv = new TextView(this);
+        tv.setText(s);
+        int myColor = Color.parseColor(hexColor);
+        tv.setBackgroundColor(myColor);
         GridLayout.Spec rowSpec = GridLayout.spec(row);
         GridLayout.Spec colSpec = GridLayout.spec(col, size, weight);
         GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, colSpec);
